35305e39596ed65ededa0e2299c1d84c35778569
Lasse Claes Jacobsen <lacj@windows-jc28cjp.kea.local>
2020-01-21 17:00:06 +0100

    three dots in js?

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 //alert("hello");
+//tjek "three dots" https://dev.to/sagar/three-dots---in-javascript-26ci
 window.addEventListener("load", () => {
-  const all = [...document.querySelectorAll("body *")];
+  /*const all = [...document.querySelectorAll("body *")];
+  console.log('all');
+  console.log(all);*/
+  const all2 = [window.document.querySelectorAll("body *")];
+  console.log('all2');
+  console.log(all2);
+  //  setInterval(() => {
+  //    all.forEach(el => {
+  //      el.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
+  //    })
+  //  }, 1000)
   setInterval(() => {
-    all.forEach(el => {
+    all2[0].forEach(el => {
       el.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
     })
   }, 1000)
